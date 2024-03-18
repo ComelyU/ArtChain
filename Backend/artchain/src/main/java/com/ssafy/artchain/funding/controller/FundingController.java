cc24d88b220f11c18bd841c773bb8179d5375443
김승우 <wskyard96@gmail.com>
2024-03-18 16:21:08 +0900

    [feat] BE : 펀딩 생성 기능 구현

--- Backend/artchain/src/main/java/com/ssafy/artchain/funding/controller/FundingController.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/funding/controller/FundingController.java
@@ -1,7 +1,15 @@
 package com.ssafy.artchain.funding.controller;
 
+import com.ssafy.artchain.funding.dto.FundingCreateRequestDto;
+import com.ssafy.artchain.funding.response.StatusCode;
+import com.ssafy.artchain.funding.service.FundingService;
+import com.ssafy.artchain.funding.response.DefaultResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class FundingController {
+
+    private final FundingService fundingService;
+
+    @PostMapping
+    public ResponseEntity<DefaultResponse<Void>> createFunding(@RequestBody FundingCreateRequestDto dto){
+        int result = fundingService.createFunding(dto);
+        if(result == -1){
+            return DefaultResponse.emptyResponse(HttpStatus.OK, StatusCode.FAIL_CREATE_FUNDING);
+        } else {
+            return DefaultResponse.emptyResponse(HttpStatus.OK, StatusCode.SUCCESS_CREATE_FUNDING);
+        }
+    }
 
 }
