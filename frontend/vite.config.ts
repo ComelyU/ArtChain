b38a97f09626d0996992b70af604d016864ce9ee
김지은 <jieun9912@gmail.com>
2024-03-22 12:34:22 +0900

    [test] INFRA : hmr host 우리 도메인으로 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,10 +9,10 @@ export default defineConfig({
     host: "0.0.0.0",
     hmr: {
       // HMR 연결을 위한 설정
-      protocol: "ws", // 보안 연결을 위해 'wss' 사용
-      // host: "j10a708.p.ssafy.io", // 도메인 이름
-      host: "localhost", // 도메인 이름
-      // port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
+      protocol: "wss", // 보안 연결을 위해 'wss' 사용
+      host: "j10a708.p.ssafy.io", // 도메인 이름
+      // host: "localhost", // 도메인 이름
+      port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
       // public: 'j10a708.p.ssafy.io:443' // 공개적으로 접근 가능한 URL (필요한 경우)
     },
   },
