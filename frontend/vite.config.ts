2d303e298d1fac0a172350ccdf89d2cade90714a
김지은 <jieun9912@gmail.com>
2024-03-22 13:23:50 +0900

    [test] INFRA : strictPort 추가, hmr clientPort 443

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       // protocol: "wss", // 보안 연결을 위해 'wss' 사용
       // host: "j10a708.p.ssafy.io", // 도메인 이름
       // port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
-      clientPort: 3000,
+      clientPort: 443,
     },
   },
 });
