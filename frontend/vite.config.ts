e2d7048450cb7ce8c7dc787d349bacf86284a4d9
김지은 <jieun9912@gmail.com>
2024-03-22 13:29:37 +0900

    [test] INFRA : hmr 삭제

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 3000,
     host: "0.0.0.0",
     strictPort: true,
-    hmr: {
-      // protocol: "wss", // 보안 연결을 위해 'wss' 사용
-      // host: "j10a708.p.ssafy.io", // 도메인 이름
-      // port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
-      clientPort: 443,
-    },
+    // hmr: {
+    //   // protocol: "wss", // 보안 연결을 위해 'wss' 사용
+    //   // host: "j10a708.p.ssafy.io", // 도메인 이름
+    //   // port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
+    //   clientPort: 443,
+    // },
   },
 });
