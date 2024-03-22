5443a2be03c4db4a1305bd14d7d1e2979124d170
김지은 <jieun9912@gmail.com>
2024-03-22 12:42:45 +0900

    [test] INFRA : hmr clientPort 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,13 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0",
-    // hmr: {
-    //   // HMR 연결을 위한 설정
-    //   protocol: "wss", // 보안 연결을 위해 'wss' 사용
-    //   host: "j10a708.p.ssafy.io", // 도메인 이름
-    //   // host: "localhost", // 도메인 이름
-    //   port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
-    //   // public: 'j10a708.p.ssafy.io:443' // 공개적으로 접근 가능한 URL (필요한 경우)
-    // },
+    hmr: {
+      // HMR 연결을 위한 설정
+      // protocol: "wss", // 보안 연결을 위해 'wss' 사용
+      // host: "j10a708.p.ssafy.io", // 도메인 이름
+      // port: 443, // 서버가 SSL을 사용하여 HTTPS를 통해 접근할 때 사용하는 포트
+      clientPort: 3000,
+    },
   },
 });
